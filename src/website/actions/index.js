3d82109e2b2020948d78bb586e7aa1c8838e305d
VinVaz <viniciusvazpro@gmail.com>
2018-09-05 23:23:22 -0300

    create first tests

---
+++ src/website/actions/index.js
@@ -0,0 +1,40 @@
+export const GET_TWEETS = 'GET_TWEETS';
+export const GET_FOLLOWING = 'GET_FOLLOWING';
+export const GET_FOLLOWERS = 'GET_FOLLOWERS';
+export const CREATE_TWEET = 'CREATE_TWEET';
+export const SEARCH_TWITTER = 'SEARCH_TWITTER';
+export const LIKE_TWEET = 'LIKE_TWEET';
+export const REPLY_TWEET = 'REPLY_TWEET';
+export const RETWEET = 'RETWEET';
+export const FOLLOW_SOMEONE = 'FOLLOW_SOMEONE';
+
+
+export const getTweets = () => ({
+  type: GET_TWEETS,
+});
+export const getFollowing = () => ({
+  type: GET_FOLLOWING,
+});
+export const getFollowers = () => ({
+  type: GET_FOLLOWERS,
+});
+export const createTweet = text => ({
+  type: CREATE_TWEET,
+  text,
+});
+export const searchTwitter = name => ({
+  type: SEARCH_TWITTER,
+  name,
+});
+export const likeTweet = () => ({
+  type: LIKE_TWEET,
+});
+export const replyTweet = () => ({
+  type: REPLY_TWEET,
+});
+export const retweet = () => ({
+  type: RETWEET,
+});
+export const followSomeone = () => ({
+  type: FOLLOW_SOMEONE,
+});
